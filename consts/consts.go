053124bad87b3739d14445f81b7d0720b21d52cc
praneelpp <142261163+praneelpp@users.noreply.github.com>
2024-01-02 23:23:01 +0530

    Update consts.go

--- consts/consts.go
+++ consts/consts.go
@@ -13,11 +13,11 @@ import (
 
 const (
 	// TODO: choose a human-readable part for your hyperchain
-	HRP = ""
+	HRP = "Kellogs"
 	// TODO: choose a name for your hyperchain
-	Name = ""
+	Name = "Cornflakes"
 	// TODO: choose a token symbol
-	Symbol = ""
+	Symbol = "KLG"
 )
 
 var ID ids.ID
